f9050a8770eec47247d95c3e23a228889cd6e61b
Hamid Javadi <javadi.business@gmail.com>
2022-06-19 15:22:06 +0300

    Add Firebase - Configure firebase - Initialized firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'hamidjavadi-56f12',
+    appId: '1:521675531703:web:de86444d876ef485f1f432',
+    storageBucket: 'hamidjavadi-56f12.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDPJP5f5QWLL3T9-j6Yy6cpUkPKvni0L9g',
+    authDomain: 'hamidjavadi-56f12.firebaseapp.com',
+    messagingSenderId: '521675531703',
+    measurementId: 'G-DDCS045104',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'hamidjavadi-56f12',
+    appId: '1:521675531703:web:de86444d876ef485f1f432',
+    storageBucket: 'hamidjavadi-56f12.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDPJP5f5QWLL3T9-j6Yy6cpUkPKvni0L9g',
+    authDomain: 'hamidjavadi-56f12.firebaseapp.com',
+    messagingSenderId: '521675531703',
+    measurementId: 'G-DDCS045104',
+  },
 };
 
 /*
